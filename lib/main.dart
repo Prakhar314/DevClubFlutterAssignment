fc421e35edde29bc5a0a9c4ebea21a88c3578e29
Prakhar <prakharjagwani7@gmail.com>
2019-12-26 00:02:14 +0530

    Circles Drawn

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:sensors/sensors.dart';
 import 'package:flutter/services.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -17,10 +26,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
-      
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    
     var a = accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
         x = event.x;
@@ -29,14 +41,59 @@ class _MyAppState extends State<MyApp> {
       });
     });
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MyApp"),
-        ),
-        body: Container(
-          child: Text("x:${this.x} z:${this.z} y:${this.y}"),
-          alignment: Alignment.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: height,
+              width: width,
+            ),
+            Positioned(
+              top: 0.1 * width,
+              left: 0.1 * width,
+              child: Container(
+                height: 0.8 * width,
+                width: 0.8 * width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(0.4 * width),
+                  border: Border.all(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0.3 * width,
+              left: 0.3 * width,
+              child: Container(
+                height: 0.4 * width,
+                width: 0.4 * width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(0.2 * width),
+                  border: Border.all(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0.3 * width,
+              left: 0.3 * width,
+              child: Container(
+                height: 0.4 * width,
+                width: 0.4 * width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(0.2 * width),
+                    color: Color.fromRGBO(120, 2, 255, 0.8)),
+              ),
+            ),
+          ],
         ),
       ),
     );
